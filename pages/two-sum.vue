7de18966b7d60cd09458870c778ce4d69c76fda4
Narek <narekkaravarichyan1910@gmail.com>
2025-06-18 00:12:09 +0400

    fixbug

--- pages/two-sum.vue
+++ pages/two-sum.vue
@@ -98,7 +98,7 @@ function handleSubmit() {
     } else {
       error.value = 'Solution not found.'
     }
-  } catch (_e) {
+  } catch  {
     error.value = 'An error occurred while processing data.'
   }
 }
